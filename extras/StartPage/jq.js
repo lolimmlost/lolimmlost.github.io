50c8f038622b8914fbd1fee3777bdc6c24e788cb
lolimmlost <johnoflol@gmail.com>
2018-03-19 00:52:34 -0700

    Start Page

---
+++ extras/StartPage/jq.js
@@ -0,0 +1,24 @@
+$("#btn1").click(function() {
+ $('body').removeClass();
+ $('body').addClass('class1');
+});
+
+$("#btn2").click(function() {
+ $('body').removeClass();
+ $('body').addClass('class2');
+});
+
+$("#btn3").click(function() {
+ $('body').removeClass();
+ $('body').addClass('class3');
+});
+
+$("#btn4").click(function() {
+ $('body').removeClass();
+ $('body').addClass('class4');
+});
+
+$("#btn5").click(function() {
+ $('body').removeClass();
+ $('body').addClass('class5');
+});
